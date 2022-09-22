1506ae1ea80bd0482b33ff88d5a5f1312603fca9
Nolan <nolan.m.mcallister@gmail.com>
2022-09-21 21:45:04 -0700

    Update main.java

--- Code/src/main.java
+++ Code/src/main.java
@@ -1,26 +1,5 @@
 public class main {
     public static void main(String[] args) {
 
-    }
-
-
-    //Bubble sort
-    public void bubblesort(){
-
-    }
-
-    //Insertion sort
-    public void insertsort(){
-
-    }
-
-    //Binary search
-    public void binarys(){
-
-    }
-
-    //Linear search
-    public void linears(){
-        
-    }
-}
+    };
+};
