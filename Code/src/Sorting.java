827e6ea779aed37435528a97cbe257ecea59cd87
Nolan <nolan.m.mcallister@gmail.com>
2022-09-21 21:32:17 -0700

    Added quicksort algorithm

--- Code/src/Sorting.java
+++ Code/src/Sorting.java
@@ -1,4 +1,22 @@
-public class Sort
+public class Sorting
 {
+    public static void quicksort(int[] arr, int lmp, int rmp)
+    {
+        if (lmp < rmp)
+        {
+            int p = lmp;
+            for (int i = lmp; i < rmp; i++)
+            {
+                if (arr[i] < arr[rmp])
+                {
+                    arr[i] = arr[i] ^ arr[p] ^ (arr[p] = arr[i]);
+                    p++;
+                };
+            };
 
-}
+            arr[rmp] = arr[rmp] ^ arr[p] ^ (arr[p] = arr[rmp]);
+            quicksort(arr, lmp, p - 1);
+            quicksort(arr, p + 1, rmp);
+        };
+    };
+};
