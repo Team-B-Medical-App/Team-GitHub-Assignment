c9e12b5bd84d7e28bf3c2a45d6266e8862e5129d
ogooluwa <ogooluwa@localhost>
2022-09-23 06:35:34 -0700

    Binary Search method

--- Code/src/searching.java
+++ Code/src/searching.java
@@ -1,16 +1,27 @@
-public class searching {
+public class searching
+{
     
-    //Linear Search
-    public static int linears(int arr[], int target) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == target) {
-
-                return i;
-            }
-
+	
+	public static int binarys(int arr[], int z)
+    {
+        int x = 0, y = arr.length - 1;
+        while (x <= y) {
+            int m = x + (y - x) / 2;
+ 
+            if (arr[m] == z)
+                return m;
+ 
+            if (arr[m] < z)
+                x = m + 1;
+ 
+            else
+                y = m - 1;
         }
-
+ 
+        
         return -1;
-
+        
     }
+	 
+	
 }
