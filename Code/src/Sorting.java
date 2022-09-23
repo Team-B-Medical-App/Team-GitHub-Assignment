78f68261e3a013efd5e8a1012aa590f5b50b3284
Zeon-restorationist <99059238+Zeon-restorationist@users.noreply.github.com>
2022-09-23 11:18:49 -0700

    Bubble sort implementation

--- Code/src/Sorting.java
+++ Code/src/Sorting.java
@@ -1,4 +1,24 @@
 public class Sorting
 {
-
+  public static ArrayList<Integer> bubSort(ArrayList in){
+          ArrayList<Integer> temp = in;
+          int hold;
+          int count = 0;
+          boolean sorted = false;
+          while(sorted!= true){
+              for (int i = 0; i < temp.size()-1; i++) {
+                  if(temp.get(i) > temp.get(i+1)){
+                      hold = temp.get(i+1);
+                      temp.set(i+1, temp.get(i));
+                      temp.set(i, hold);
+                      count ++;
+                  }
+              }
+              if(count == 0){
+                  break;
+              }
+              count = 0;
+          }
+          return temp;
+      }
 }
