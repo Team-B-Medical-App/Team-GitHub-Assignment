945d52b87d6a72c6eb9b0dfad542e8f2a53d4d2f
Zeon-restorationist <99059238+Zeon-restorationist@users.noreply.github.com>
2022-09-23 19:29:36 -0700

    re-added tests and fixed directory

---
+++ Code/src/test/searchingTests.java
@@ -0,0 +1,52 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.*;
+
+    public class searchingTests
+    {
+
+        public Random random = new Random();
+
+        @Test
+        public void linearSearchTest()
+        {
+            for (int n = 1; n < 5; n++)
+            {
+                int[] arr = new int[10 ^ n];
+                Arrays.setAll(arr, i -> i + 1);
+
+                for (int i = 0; i < arr.length - 1; i++)
+                {
+                    int p = random.nextInt(i + 1);
+                    arr[i] = arr[i] ^ arr[p] ^ (arr[p] = arr[i]);
+                };
+
+                int index = random.nextInt(arr.length);
+                int target = arr[index];
+
+                assertEquals(searching.linears(arr, target), index);
+            };
+        };
+
+        @Test
+        public void binarySearchTest()
+        {
+            for (int n = 1; n < 5; n++)
+            {
+                int[] arr = new int[10 ^ n];
+                Arrays.setAll(arr, i -> i + 1);
+
+                for (int i = 0; i < arr.length - 1; i++)
+                {
+                    int p = random.nextInt(i + 1);
+                    arr[i] = arr[i] ^ arr[p] ^ (arr[p] = arr[i]);
+                };
+
+                int index = random.nextInt(arr.length);
+                int target = arr[index];
+
+                assertEquals(searching.binarys(arr, target), index);
+            };
+        };
+    };
+}
---
+++ Code/src/test/sortingTests.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.*;
+
+public class sortingTests {
+
+    public Random random = new Random();
+
+    @Test
+    public void quickSortTest()
+    {
+        for (int n = 1; n < 5; n++)
+        {
+            int[] arr = new int[10 ^ n];
+            Arrays.setAll(arr, i -> i + 1);
+            int [] answer = arr.clone();
+
+            for (int i = 0; i < arr.length - 1; i++)
+            {
+                int p = random.nextInt(i + 1);
+                arr[i] = arr[i] ^ arr[p] ^ (arr[p] = arr[i]);
+            };
+
+            Sorting.quicksort(arr, 0, arr.length - 1);
+
+            assertArrayEquals(arr, answer);
+        };
+    };
+
+    @Test
+    public void bubbleSortTest()
+    {
+        for (int n = 1; n < 5; n++)
+        {
+            int[] arr = new int[10 ^ n];
+            Arrays.setAll(arr, i -> i + 1);
+            int [] answer = arr.clone();
+
+            for (int i = 0; i < arr.length - 1; i++)
+            {
+                int p = random.nextInt(i + 1);
+                arr[i] = arr[i] ^ arr[p] ^ (arr[p] = arr[i]);
+            };
+
+            Sorting.bubblesort(arr, 0, arr.length - 1);
+
+            assertArrayEquals(arr, answer);
+        };
+    };
+};
